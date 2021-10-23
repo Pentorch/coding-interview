40e1c343423d6c08da4251136fe9423861f6e90a
WillyAgustinoEfendi <59289325+Pentorch@users.noreply.github.com>
2021-10-24 06:39:56 +0700

    update Logic-Test-Reverse

--- Logic-Test/ReverseWords/reversewords.js
+++ Logic-Test/ReverseWords/reversewords.js
@@ -1,6 +1,40 @@
-function reverse(form) {
-  let text = [];
-  let str = form.revtext.value;
-  for (i = 0; i <= str.length; i++) text = str.substring(i, i + 1) + text;
-  form.revtext.value = text;
+document.getElementById("btnReverse").onclick = function () {
+  reverse();
+};
+
+String.prototype.replaceAt = function (index, replacement) {
+  if (index >= this.length) {
+    return this.valueOf();
+  }
+
+  return this.substring(0, index) + replacement + this.substring(index + 1);
+};
+
+function reverseWord(word) {
+  let wordLength = word.length;
+  let reversedWord = word;
+  for (let j = 0; j < wordLength / 2; j++) {
+    let temp = reversedWord[j];
+
+    reversedWord = reversedWord.replaceAt(j, word[wordLength - 1 - j]);
+    reversedWord = reversedWord.replaceAt(wordLength - 1 - j, temp);
+  }
+  return reversedWord;
+}
+
+function reverse() {
+  let text = document.getElementById("text").value;
+
+  let splittedText = text.split(" ");
+
+  let reversedWords = [];
+
+  for (let i = 0; i < splittedText.length; i++) {
+    let word = splittedText[i];
+
+    let reversedWord = reverseWord(word);
+    reversedWords.push(reversedWord);
+  }
+
+  document.getElementById("demo").innerHTML = reversedWords.join(" ");
 }
